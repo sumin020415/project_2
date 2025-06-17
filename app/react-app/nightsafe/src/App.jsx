183150524707efbc4adff5ccfd83b4f7d14055e6
JeongHui-seong <jgmltjd99@naver.com>
2025-06-18 01:31:29 +0900

    홈 화면 아이콘 삽입 및 해당 위치에 마커 생성

--- app/react-app/nightsafe/src/App.jsx
+++ app/react-app/nightsafe/src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 import Home from './pages/Home'
 import Posts from './pages/Posts'
-import SearchBar from './components/SearchBar'
 import { NavLink, Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
  
 function App() {
   return (
@@ -12,7 +10,6 @@ function App() {
       <NavLink to='/' className='logo'>LOGO</NavLink>
       <nav id='nav'>
         <ul>
-          <SearchBar className='nav_searchbar' />
           <li><NavLink to='/' className={({ isActive }) => isActive ? 'nav_active' : ''}>홈</NavLink></li>
           <li><NavLink to='/post' className={({ isActive }) => isActive ? 'nav_active' : ''}>제보</NavLink></li>
         </ul>
