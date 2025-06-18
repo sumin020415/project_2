ac6971d1a2f371da8947727f984e3c0008aaf169
dongho <dongho7736@gmail.com>
2025-06-18 09:34:31 +0900

    .

--- app/react-app/nightsafe/src/App.jsx
+++ app/react-app/nightsafe/src/App.jsx
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import './App.css'
 import Home from './pages/Home'
 import Posts from './pages/Posts'
+import Login from './pages/Login'
 import SearchBar from './components/SearchBar'
 import { NavLink, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
@@ -16,46 +16,17 @@ function App() {
           <SearchBar className='nav_searchbar' />
           <li><NavLink to='/' className={({ isActive }) => isActive ? 'nav_active' : ''}>홈</NavLink></li>
           <li><NavLink to='/post' className={({ isActive }) => isActive ? 'nav_active' : ''}>제보</NavLink></li>
+          <li><NavLink to='/login' className={({ isActive }) => isActive ? 'nav_active' : ''}>로그인</NavLink></li>
         </ul>
       </nav>
     </header>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/post' element={<Posts/>} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </>
   )
 }
 
 export default App
-=======
-import './App.css'
-import Home from './pages/Home'
-import Posts from './pages/Posts'
-import SearchBar from './components/SearchBar'
-import { NavLink, Routes, Route } from 'react-router-dom'
-import { useState } from 'react'
- 
-function App() {
-  return (
-    <>
-    <header id='header'>
-      <NavLink to='/' className='logo'>LOGO</NavLink>
-      <nav id='nav'>
-        <ul>
-          <SearchBar className='nav_searchbar' />
-          <li><NavLink to='/' className={({ isActive }) => isActive ? 'nav_active' : ''}>홈</NavLink></li>
-          <li><NavLink to='/post' className={({ isActive }) => isActive ? 'nav_active' : ''}>제보</NavLink></li>
-        </ul>
-      </nav>
-    </header>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/post' element={<Posts/>} />
-      </Routes>
-    </>
-  )
-}
-
-export default App
->>>>>>> b4690527f6626a1a51ae79e8a94cb0bc047049a3
